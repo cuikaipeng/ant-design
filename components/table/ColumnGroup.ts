e64e38831eec9eaaa8552212e256418e355e0bd7
afc163 <afc163@gmail.com>
2024-09-09 14:35:32 +0800

    remove /* istanbul ignore next */

--- components/table/ColumnGroup.ts
+++ components/table/ColumnGroup.ts
@@ -11,9 +11,6 @@ export interface ColumnGroupProps<RecordType = AnyObject>
     | React.ReactElement<ColumnProps<RecordType>>[];
 }
 
-/* istanbul ignore next */
-/** This is a syntactic sugar for `columns` prop. So HOC will not work on this. */
-
 const ColumnGroup = <RecordType extends AnyObject>(_: ColumnGroupProps<RecordType>) => null;
 
 export default ColumnGroup;
